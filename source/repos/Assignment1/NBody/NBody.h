d329b380ce4de1397475fcd76a228e6c52ba9a88
Edward Pearce <38229933+edwardmpearce@users.noreply.github.com>
2020-05-18 15:24:44 +0100

    Replace softening factor by its precalculated square to save computation

--- source/repos/Assignment1/NBody/NBody.h
+++ source/repos/Assignment1/NBody/NBody.h
@@ -2,11 +2,13 @@
 #ifndef __NBODY_HEADER__
 #define __NBODY_HEADER__
 
-#define G			9.8f		// Gravitational constant
-#define dt			0.01f		// Time step
-#define SOFTENING	2.0f		// Softening parameter to help with numerical instability
+/* Our gravitational constant differs from https://en.wikipedia.org/wiki/Gravitational_constant and is closer to 
+https://en.wikipedia.org/wiki/Acceleration_due_to_gravity in value, but our distances are normalised anyway */
+#define G      9.8f  // Gravitational constant
+#define dt     0.01f // Time step
+#define eps_sq 4.0f  // Softening parameter to help with numerical instability
 
-struct nbody{
+struct nbody{ // Changed to structure of arrays to facilitate coalesced memory access
 	float *x, *y, *vx, *vy, *m;
 };
 
